4b398c08ba659c878c06dcb8bcae91c2f77621b7
Avoler0 <jbu1391@gmail.com>
2021-12-01 08:38:18 +0900

    notetaking_start

---
+++ NoteScript.js
@@ -0,0 +1,8 @@
+const SaveIcon = document.querySelector(".SaveIcon");
+const noteField = document.querySelector(".noteField");
+const EditNote = document.querySelector(".EditNote");
+function ClickPlusNote(){
+  document.write(noteField,EditNote);
+}
+
+//SaveIcon.addEventListener("click",ClickPlusNote);
