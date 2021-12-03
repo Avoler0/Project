55476e27ec95e8f00706ace77c709aface9a01c6
Avoler0 <jbu1391@gmail.com>
2021-12-03 12:15:20 +0900

    noteFirst

--- NoteScript.js
+++ NoteScript.js
@@ -1,8 +1,52 @@
 const SaveIcon = document.querySelector(".SaveIcon");
 const noteField = document.querySelector(".noteField");
 const EditNote = document.querySelector(".EditNote");
+//const SaveNote = document.querySelector(".SaveNote");
 function ClickPlusNote(){
-  document.write(noteField,EditNote);
+  if(!document.querySelector(".EditNote")){
+    CreateNote();
+}else{
+  ReNote();
 }
+}
+function CreateNote(){
+  let NoteDiv = document.createElement('div');
+  let NoteBtn = document.createElement('Button');
+  NoteDiv.className ="EditNote";
+  NoteDiv.setAttribute('contenteditable','true');
+  NoteDiv.setAttribute('onkeydown','KetEvent(event)');
 
+  NoteBtn.className = "SaveBtn";
+  NoteBtn.setAttribute('onclick','NoteSave()');
+  NoteBtn.innerHTML = "버튼";
+  noteField.appendChild(NoteDiv);
+  noteField.appendChild(NoteBtn);
+}
+function ReNote(){
+  let OkNo = confirm("새로 만드시겠습니까?");
+  if(OkNo)
+  {
+    ClickPlusNote();
+  }else{
+    alert("안실행");
+  }
+}
+function NoteSave(){
+  
+    let NoteValue = document.querySelector('.EditNote');
+    let SaveNoteFile = document.createElement('div');
+    SaveNoteFile.className ="SaveNote";
+    SaveNoteFile.innerHTML = NoteValue.innerHTML;
+    
+    noteField.appendChild(SaveNoteFile);
+  
+  
+  
+}
 //SaveIcon.addEventListener("click",ClickPlusNote);
+
+function KetEvent(Event){
+  if(Event.keyCode==13){
+    NoteSave();
+  }
+}
--- TodoScript.js
+++ TodoScript.js
@@ -1,7 +1,6 @@
 const Todo_Text = document.querySelector(".todoText"); // 할일 적는 input
 const Todo_ListUl = document.querySelector(".TodoList"); // ul 리스트 클래스
 const Timer = document.querySelector(".timer");
-
 let TodoData = new Array();
 let cnt = 0;
 let time;
@@ -45,46 +44,65 @@ function CreateTodo(){
   }
 }
 function init(){
-  const Temp_ListCnt = localStorage.getItem('ListCnt')
+  const Temp_ListCnt = localStorage.getItem('ListCnt');
+
   if(Temp_ListCnt == null){
     return;
   }else{
   for(let i=0; i<=Temp_ListCnt; i++){
     let lis = document.createElement('li'); // li 생성
+    const Temp_Check = localStorage.getItem("CheckTodo"+i);
     const Temp_Data = localStorage.getItem("TodoData"+i);
+    if(!Temp_Data){
+      break;
+    }
+    
     lis.setAttribute("class","list-group-item"); // 새로 생성된 태그에 class 추가
     lis.setAttribute("id","li"+i); // 새로 생성된 태그에 id 추가
     lis.innerHTML = Temp_Data;// 새로 생성된 현재 값에 value값 추가
     lis.innerHTML += "<a href='#' class ='RemoveBtn' onclick='RemoveTodo("+cnt+")'><i class='bx bx-trash'></i></a>"
     lis.innerHTML += "<a href='#' class ='CheckTodo' onclick='CheckTodo("+cnt+")'><i class='bx bx-check'></i></a>"
     Todo_ListUl.appendChild(lis);
+    if(Temp_Check){
+      //CheckTodo(i);
+      lis.style.textDecoration="line-through";
+      lis.classList.toggle("Enable");
+    }
     cnt++; // 입력한게 몇개인지
       }
     }
   }
 
-function RemoveLocal(){
+/*function RemoveLocal(){
   localStorage.clear();
   //localStorage.setItem('ListCnt',0);
-}
+}*/
 function RemoveTodo(count){
-
+  if(count === cnt+1){
+    count--;
+  }
   const lis = document.querySelector("#li"+count);
-  console.log("Cnt = "+cnt +"Count = "+count);
-
   Todo_ListUl.removeChild(lis);
+  localStorage.setItem('ListCnt',count);
   localStorage.removeItem("TodoData"+count);
-  
+  localStorage.removeItem("CheckTodo"+count);
+  console.log(count);
   if(cnt == 0 || count == 0){
-    //localStorage.removeItem('ListCnt');
     localStorage.clear();
   }
   
 }
 function CheckTodo(cnt){
   let lis = document.querySelector("#li"+cnt);
-  lis.style.textDecoration="line-through";
-  lis.classList.toggle("Enable");
+  if(lis.style.textDecoration == "line-through"){
+    lis.style.textDecoration="none";
+    localStorage.removeItem("CheckTodo"+cnt);
+  }
+  else{
+    localStorage.setItem("CheckTodo"+cnt,true);
+    lis.style.textDecoration="line-through";
+    lis.classList.toggle("Enable");
+  }
 }
 
 function KeyEvent(e) {
